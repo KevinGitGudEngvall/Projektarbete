f3f868cecdd870558c035be7d6d76f8a7dada5ca
Semiturdean <Semiturdean@hotmail.com>
2018-05-08 14:15:20 +0200

    Refactor

--- src/main/java/se/group/projektarbete/repository/UserRepository.java
+++ src/main/java/se/group/projektarbete/repository/UserRepository.java
@@ -3,11 +3,8 @@ package se.group.projektarbete.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import se.group.projektarbete.data.User;
-<<<<<<< HEAD
-=======
 
 import java.util.List;
->>>>>>> 39b73c7ce10cca3edea2581baa64f8bf978e6b0e
 import java.util.Optional;
 
 
@@ -15,16 +12,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     @Query("SELECT MAX(userNumber) from User")
     Optional<Long> getHighestUserNumber();
-<<<<<<< HEAD
     Optional<User> findUserByuserNumber(Long id);
-=======
-
-   Optional<User> findUserByuserNumber(Long id);
-
-   List<User> findAll();
-
-
-
-
->>>>>>> 39b73c7ce10cca3edea2581baa64f8bf978e6b0e
+    List<User> findAll();
 }
