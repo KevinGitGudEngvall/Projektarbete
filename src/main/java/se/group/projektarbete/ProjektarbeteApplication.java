5d9d9483d8bc9844d3d7aa7bd15136bb59060b45
Senad Hasic <senad.hasic@gmail.com>
2018-05-09 13:19:08 +0200

    fourth
    commit

--- src/main/java/se/group/projektarbete/ProjektarbeteApplication.java
+++ src/main/java/se/group/projektarbete/ProjektarbeteApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import se.group.projektarbete.data.Team;
 import se.group.projektarbete.data.User;
 import se.group.projektarbete.data.WorkItem;
 import se.group.projektarbete.data.workitemenum.Status;
@@ -14,13 +13,13 @@ import se.group.projektarbete.repository.WorkItemRepository;
 import se.group.projektarbete.service.TeamService;
 
 @SpringBootApplication
-public class ProjektarbeteApplication {
+public class ProjektarbeteApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjektarbeteApplication.class, args);
 	}
 
-	/*@Autowired
+	@Autowired
 	UserRepository userRepository;
 
 	@Autowired
@@ -34,16 +33,16 @@ public class ProjektarbeteApplication {
 
 	@Override
 	public void run(String... args) throws Exception {
-		User user1 = userRepository.save(new User("1", "1","1", 1000L, true));
-		User user2 = userRepository.save(new User("2", "2","2", 1001L, true));
-		User user3 = userRepository.save(new User("3", "3","3", 1002L, true));
-		User user4 = userRepository.save(new User("4", "4","4", 1003L, true));
+
+		User user1 = userRepository.save(new User("1", "1","1", 1000L));
+		User user2 = userRepository.save(new User("2", "2","2", 1001L));
+		User user3 = userRepository.save(new User("3", "3","3", 1002L));
+		User user4 = userRepository.save(new User("4", "4","4", 1003L));
 
 		WorkItem workItem1 = workItemRepository.save(new WorkItem("W1", "W1", Status.UNSTARTED, user1));
 		WorkItem workItem2 = workItemRepository.save(new WorkItem("W2", "W2", Status.UNSTARTED, user2));
 		WorkItem workItem3 = workItemRepository.save(new WorkItem("W3", "W3", Status.UNSTARTED, user3));
 		WorkItem workItem4 = workItemRepository.save(new WorkItem("W4", "W4", Status.UNSTARTED, user4));
-<<<<<<< HEAD
-	}*/
+	}
 
 }
